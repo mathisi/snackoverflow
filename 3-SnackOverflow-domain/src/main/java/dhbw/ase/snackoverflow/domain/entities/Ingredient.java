e80b1487e0396561c71037925c8423a258492303
Mathis Imming <mathis.imming@sap.com>
2025-02-20 09:11:45 +0100

    recipe-entity add standard methods

--- 3-SnackOverflow-domain/src/main/java/dhbw/ase/snackoverflow/domain/entities/Ingredient.java
+++ 3-SnackOverflow-domain/src/main/java/dhbw/ase/snackoverflow/domain/entities/Ingredient.java
@@ -6,4 +6,43 @@ public class Ingredient {
     private int id;
     private Metric metric;
     private String name;
+
+    public Ingredient(int id, Metric metric, String name) {
+        this.id = id;
+        this.metric = metric;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Metric getMetric() {
+        return metric;
+    }
+
+    public void setMetric(Metric metric) {
+        this.metric = metric;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "id=" + id +
+                ", metric=" + metric +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- 3-SnackOverflow-domain/src/main/java/dhbw/ase/snackoverflow/domain/entities/ProcessStep.java
+++ 3-SnackOverflow-domain/src/main/java/dhbw/ase/snackoverflow/domain/entities/ProcessStep.java
@@ -1,5 +1,37 @@
 package dhbw.ase.snackoverflow.domain.entities;
 
-public class ProcessStep {
+import java.util.Arrays;
 
+public class ProcessStep {
+    private Ingredient[] ingredients;
+    private String description;
+
+    public ProcessStep(Ingredient[] ingredients, String description) {
+        this.ingredients = ingredients;
+        this.description = description;
+    }
+
+    public Ingredient[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Ingredient[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "ProcessStep{" +
+                "ingredients=" + Arrays.toString(ingredients) +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
